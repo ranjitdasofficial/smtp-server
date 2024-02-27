36c77ed1abcb0fcc95edceb69dc8d8a581c797c2
technicalranjitofficial <dranjitkumar16@gmail.com>
2024-02-27 13:58:09 +0530

    mail server

---
+++ index.js
@@ -0,0 +1,28 @@
+const SMTPServer = require("smtp-server").SMTPServer;
+
+
+const server = new SMTPServer({
+    allowInsecureAuth: true,
+    authOptional: true,
+    onConnect(session, callback) {
+        console.log("Connected");
+        return callback();
+    },
+
+    onMailFrom(address, session, callback) {
+        console.log("Mail from:", address.address);
+        return callback();
+    },
+
+    onRcptTo(address, session, callback) {
+        console.log("Rcpt to:", address.address);
+        return callback();
+    },
+
+    onData(stream, session, callback) {
+        stream.pipe(process.stdout);
+        stream.on("end", callback);
+    }
+
+        
+})
